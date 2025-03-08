2ca751454d1844055483d0d98d0e94111e84ad8c
JinXzAF <arianf2004@gmail.com>
2025-03-07 20:13:44 -0500

    changed vector to Point

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Point/Point.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.se2aa4.island.teamXXX.Point;
+
+public class Point {
+    public Integer x;
+    public Integer y;
+
+    public Point(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
